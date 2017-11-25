f43fa72b3215f3c20eb2d41ad2eac2759bb6dea5
sushruthn96 <33973441+sushruthn96@users.noreply.github.com>
2017-11-26 00:35:40 +0530

    Add files via upload

--- Scrap.java
+++ Scrap.java
@@ -1,19 +1,63 @@
-package com.example.ranja.myapplication;
+package com.example.suthirthanagesh.buyer;
 
-import android.os.Bundle;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * Created by ranja on 11/25/2017.
- */
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Scrap extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scraplist);
+        setContentView(R.layout.activity_scrap);
+       // Log.v("DB","Connected!!!!!");
+        int value = getIntent().getExtras().getInt("bucketno");
+        //Log.v("db",""+value);
+        List i;
+
+        try {
+            i = new retrieve().execute(value).get();
+            TextView text=(TextView) findViewById(R.id.scraptext);
+           int g=i.size();
+           g=g-1;
+           int y=0;
+           String st="\n";
+           while(g>-1)
+           {
+                st= st +" " +(String) i.get(g);
+               Log.i("dis",""+st);
+
+               y++;
+               if(y%3==0)
+               {
+                   y=0;
+                   Button myButton = new Button(this);
+                   myButton.setText(st);
+                   LinearLayout ll = (LinearLayout)findViewById(R.id.scraptext);
+                   LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                   ll.addView(myButton, lp);
+                   st="\n";
+               }
+               g--;
+
+           }
+
+            //Log.i("ret",""+i.get(0));
 
 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ retrieve.java
@@ -0,0 +1,74 @@
+package com.example.suthirthanagesh.buyer;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by suthirtha Nagesh on 11/25/2017.
+ */
+
+public class retrieve extends AsyncTask<Integer, Void, List> {
+
+    private static final String URL = "jdbc:mysql://10.24.32.31";
+    private static final String user = "root";
+    private static final String password = "";
+    @Override
+    protected List doInBackground(Integer... i) {
+
+        // Do some background work
+        Log.d(TAG, "Connecting....");
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(URL, user, password);
+            con.setCatalog("tricon");
+            Statement st = con.createStatement();
+            Log.v("DB","Connected!!!!!");
+            ResultSet rs=st.executeQuery("SELECT * FROM  private");
+           // String id=rs.getString("Mobile");
+
+            int i1=(int)i[0];
+            String s;
+            if(i1==1)
+                s="Paper";
+            else if(i1==2)
+                s="Plastic";
+            else if(i1==3)
+                s="Glass";
+            else
+                s="Organic";
+            List<String> zoom = new ArrayList<>();
+
+            while(rs.next()) {
+
+                int id = rs.getInt(s);
+                if(id>0)
+                {
+               zoom.add(rs.getString("Mobile"));
+               zoom.add(rs.getString("Address"));
+               zoom.add(Integer.toString(id));
+                }
+
+            }
+            return zoom;
+
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
