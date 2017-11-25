8155bf79ba5b000e30e0ce8e9dce3ba9564346f6
ranjanp75 <ranjanp75@gmail.com>
2017-11-26 01:45:30 +0530

    Add files via upload

--- Category.java
+++ Category.java
@@ -1,16 +1,30 @@
 package com.example.ranja.myapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
 
 /**
  * Created by ranja on 11/25/2017.
  */
 
 public class Category extends AppCompatActivity {
-
+    private String mobile ;
+    private String add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +37,99 @@ public class Category extends AppCompatActivity {
 
     public void garbage(View v)
     {
+        String URL = "jdbc:mysql://10.24.32.31";
+        String user = "root";
+        String password = "";
+
+
+
+
+        try {
+
+            final String mobileNumber = this.getMobile();
+            final String address = this.getAdd();
+            Log.v("From Category:",mobileNumber);
+
+            java.sql.Connection con = (java.sql.Connection) new com.example.ranja.myapplication.Connection().execute(URL, user, password).get();
+            Log.v("DB", "Hey I am connected to make a log on garbage !!!!");
+
+            AsyncTask task =  new AsyncTask() {
+                int flag = 0;
+                @Override
+                protected Integer doInBackground(Object[] objects) {
+                    try
+                    {
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        Date date = new Date();
+                        String date1 = dateFormat.format(date);
+                        java.sql.Connection con = ((java.sql.Connection)objects[0]);
+                        Log.v("mobile number",mobileNumber);
+                        Statement st = con.createStatement();
+                        ResultSet rs = st.executeQuery("SELECT * FROM " + "govt" + " WHERE Mobile = " + "'"+mobileNumber+"' AND date = "+"'"+date1+"'");
+
+                        if(rs.next()==false)
+                        {
+
+                            st.executeUpdate("INSERT INTO govt (Mobile,Address,date,Pickup) VALUE  ('"+mobileNumber+"','"+address+"','"+date1+"','1')");
+                            Log.v("Govt:", "Entry Successfully added to govt!!!!!!!!!!!");
+                            flag = 1;
+                        }
+
+
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                    return flag;
+                }
+
+                @Override
+                protected void onPostExecute(Object o) {
+                    if (flag == 0) {
+                        Toast.makeText(getApplicationContext(), "You have already ticked for today", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Updated your response.Thank you :)", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }.execute(con);
+
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
     }
+
+    private String getMobile()
+    {
+        Bundle extras = getIntent().getExtras();
+         String mobileNumber = extras.getString("mobile");
+        return mobileNumber;
+
+    }
+
+    private String getAdd()
+    {   Bundle extras = getIntent().getExtras();
+        String address = extras.getString("address");
+        return address;
+    }
+
 
     public void scrap(View v)
     {
         Intent intent1 = new Intent(this, Scrap.class);
 
+        Bundle extras = new Bundle();
+        extras.putString("mobile",this.getMobile());
+        extras.putString("address",this.getAdd());
+        intent1.putExtras(extras);
         startActivity(intent1);
+
     }
 }
--- Scrap.java
+++ Scrap.java
@@ -1,63 +1,138 @@
-package com.example.suthirthanagesh.buyer;
+package com.example.ranja.myapplication;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+/**
+ * Created by ranja on 11/25/2017.
+ */
 
 public class Scrap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrap);
-       // Log.v("DB","Connected!!!!!");
-        int value = getIntent().getExtras().getInt("bucketno");
-        //Log.v("db",""+value);
-        List i;
+        setContentView(R.layout.scraplist);
+    }
 
+
+    public void done(View v)
+    {
+        int [] a = new int[4];
+        ViewGroup row = (ViewGroup) v.getParent();
+        for(int i =0;i<4;i++) {
+            ViewGroup l1 = (ViewGroup) row.getChildAt(i);
+            int res1 = Integer.parseInt(((TextView) l1.getChildAt(2)).getText().toString());
+            if(res1>0)
+            {
+                a[i] = res1;
+            }
+            else
+            {
+                a[i] = 0;
+            }
+        }
+        final int [] b = a;
         try {
-            i = new retrieve().execute(value).get();
-            TextView text=(TextView) findViewById(R.id.scraptext);
-           int g=i.size();
-           g=g-1;
-           int y=0;
-           String st="\n";
-           while(g>-1)
-           {
-                st= st +" " +(String) i.get(g);
-               Log.i("dis",""+st);
+            String URL = "jdbc:mysql://10.24.32.31";
+            String user = "root";
+            String password = "";
+            Bundle extras = getIntent().getExtras();
+            final  String mobileNumber = extras.getString("mobile");
+            final String address = extras.getString("address");
 
-               y++;
-               if(y%3==0)
-               {
-                   y=0;
-                   Button myButton = new Button(this);
-                   myButton.setText(st);
-                   LinearLayout ll = (LinearLayout)findViewById(R.id.scraptext);
-                   LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                   ll.addView(myButton, lp);
-                   st="\n";
-               }
-               g--;
+            Log.v("From Category:",mobileNumber);
 
-           }
+            java.sql.Connection con = (java.sql.Connection) new com.example.ranja.myapplication.Connection().execute(URL, user, password).get();
+            Log.v("DB", "Hey I am connected to make a log for scrap !!!!");
 
-            //Log.i("ret",""+i.get(0));
+            AsyncTask task =  new AsyncTask() {
+                int flag = 0;
+                @Override
+                protected Integer doInBackground(Object[] objects) {
+                    try
+                    {
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        Date date = new Date();
+                        String date1 = dateFormat.format(date);
+                        java.sql.Connection con = ((java.sql.Connection)objects[0]);
+                        Log.v("mobile number",mobileNumber);
+                        Statement st = con.createStatement();
+                        ResultSet rs = st.executeQuery("SELECT * FROM " + "private" + " WHERE Mobile = " + "'"+mobileNumber+"' AND date = "+"'"+date1+"'");
+
+                        if(rs.next()==false)
+                        {
+
+                            st.executeUpdate("INSERT INTO private (Mobile,Address,Paper,Plastic,Glass,Organic,date,Accept) VALUE  ('"+mobileNumber+"','"+address+"','"+b[0]+"','"+b[1]+"','"+b[2]+"','"+b[3]+"','"+date1+"','1')");
+                            Log.v("Private:", "Entry Successfully added to private!!!!!!!!!!!");
+                            flag = 1;
+                        }
 
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                    return flag;
+                }
+
+                @Override
+                protected void onPostExecute(Object o) {
+                    if (flag == 0) {
+                        Toast.makeText(getApplicationContext(), "You have already placed order", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Updated your response.Thank you :)", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }.execute(con);
+
+
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
 
+    }
+
+
+    public void plus(View v)
+    {
+
+        ViewGroup row = (ViewGroup) v.getParent();
+        TextView res = (TextView) row.getChildAt(2);
+        res.setText(Integer.toString(Integer.parseInt(res.getText().toString())+1) );
+
+    }
+
+    public void minus(View v)
+    {
+        ViewGroup row = (ViewGroup) v.getParent();
+        TextView res = (TextView) row.getChildAt(2);
+        int x = Integer.parseInt(res.getText().toString())-1;
+        if(x >= 0) {
+            res.setText(Integer.toString(x));
+        }
     }
 }
